3324f8229e37fa5d591010cb721011e2dc1042bb
iris damani <iris.damani@auth0.com>
2022-07-16 07:40:46 +0200

    add new features for the sorting of APIs

--- controllers/products.js
+++ controllers/products.js
@@ -10,7 +10,7 @@ res.status(200).json(products)
 
 //get All Products
 const getAllProducts = async (req, res) =>{
-   const { featured, company, name, sort} = req.query
+   const { featured, company, name, sort, fields} = req.query
    const queryObject = {}
    if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
@@ -29,7 +29,15 @@ if (sort) {
   } else {
     result = result.sort('createdAt');
   }
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ');
+    result = result.select(fieldsList);
+  }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
 
+  result = result.skip(skip).limit(limit);
     const getAllProducts = await result;
     res.status(200).json({getAllProducts, nbHits:getAllProducts.length})
 }
